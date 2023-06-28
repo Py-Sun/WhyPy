4501825af3b8ab3425bc844f3a67b83c40d57142
Hyun_W <69100145+yehang218@users.noreply.github.com>
2023-06-28 16:54:04 +0900

    📌 프로젝트 초기 설정

---
+++ WhyPyProjdect/src/main/java/com/example/whypyprojdect/WhyPyProjdectApplication.java
@@ -0,0 +1,13 @@
+package com.example.whypyprojdect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhyPyProjdectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhyPyProjdectApplication.class, args);
+	}
+
+}
---
+++ WhyPyProjdect/src/test/java/com/example/whypyprojdect/WhyPyProjdectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.whypyprojdect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhyPyProjdectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
