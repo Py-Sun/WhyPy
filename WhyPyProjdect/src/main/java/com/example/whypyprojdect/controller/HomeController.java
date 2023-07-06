536c0187b6662f9feb17ef369c29eb2ece839ec7
ksb3458 <86918962+ksb3458@users.noreply.github.com>
2023-07-06 16:49:52 +0900

    📌 html, js, css 파일 연결 테스트

---
+++ WhyPyProjdect/src/main/java/com/example/whypyprojdect/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.example.whypyprojdect.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    //GET, POST 모두
+    @RequestMapping("/")
+    public String home() {
+        return "home";
+    }
+}
---
+++ WhyPyProjdect/src/main/java/com/example/whypyprojdect/controller/LoginController.java
@@ -0,0 +1,28 @@
+package com.example.whypyprojdect.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+    //GET
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login-form";
+    }
+
+    //POST
+    @PostMapping("/login")
+    public String loginProcess(
+            @RequestParam String id,
+            @RequestParam String password,
+            Model model ) {
+        if (id.equals(password)) {
+            model.addAttribute("loginId", id);
+        }
+        return "login-result";
+    }
+}
