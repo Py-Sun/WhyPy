b39e4a2823312a70bea62453a80a1f615cb16610
ykimnida <sssdf0910@gmail.com>
2023-07-08 01:32:21 +0900

    커밋 연습. 임시 시큐리티 작성

--- WhyPyProjdect/src/main/java/com/example/whypyprojdect/config/SecurityConfig.java
+++ WhyPyProjdect/src/main/java/com/example/whypyprojdect/config/SecurityConfig.java
@@ -11,7 +11,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
         http.authorizeRequests()
                 .antMatchers("/", "/user/**", "/image/**", "/subscribe/**", "/comment/**").authenticated()
                  .anyRequest().permitAll()
