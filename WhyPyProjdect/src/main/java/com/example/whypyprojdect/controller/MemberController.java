838e22de1020257197cf004dbc0731a255a05944
ykimnida <sssdf0910@gmail.com>
2023-07-08 20:08:38 +0900

    회원가입 디비 연동

--- WhyPyProjdect/src/main/java/com/example/whypyprojdect/controller/MemberController.java
+++ WhyPyProjdect/src/main/java/com/example/whypyprojdect/controller/MemberController.java
@@ -1,13 +1,20 @@
 package com.example.whypyprojdect.controller;
 
+import com.example.whypyprojdect.dto.MemberDto;
+import com.example.whypyprojdect.service.MemberService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.jdbc.core.SqlOutParameter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+@RequiredArgsConstructor
 public class MemberController {
+    //생성자 주입. 자동적으로 서비스 클래스의 객체를 주입을 받는다. 메소드등을 사용할 수 있게 됨
+    private final MemberService memberService;
     //회원가입 페이지 출력 요청
 
     @GetMapping("/member/save")
@@ -15,7 +22,20 @@ public class MemberController {
         return "Save";
     }
 
-    @PostMapping("/member/save")  //입력받은 값 저장
+    @PostMapping("/member/save")  //입력받은 회원 가입 정보값들이 dto에 잘 담기게 됨(정리한 코드)
+    public String save(@ModelAttribute MemberDto memberDto) {
+        System.out.println("MemberController.save");
+        System.out.println("memberDto = " +memberDto);
+
+        /* MemberService memberService=new MemberService(); //memberservice 객체 생성
+        memberService.save(); //memberservice가 가진 메소드 호출
+        >> 생성자 주입 방식으로 변경하여 이 코드 삭제*/
+
+        memberService.save(memberDto); //memberService의 save메소드를 미리 정해봄 어떻게 호출할지
+
+        return "home";
+    }
+    /* (정리 전 코드_의미 파악 위해 놔둠)
     public String save(@RequestParam("memberEmail") String memberEmail,
                        @RequestParam("memberPassword") String memberPassword,
                        @RequestParam("memberName") String memberName) {
@@ -23,4 +43,6 @@ public class MemberController {
             System.out.println("memberEmail = " + memberEmail); //매개변수 생성
             return "home";
         }
+        */
+
 }
