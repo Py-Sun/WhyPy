6fa7465297695648b8e59aa720d804ab50a20e5f
ykimnida <sssdf0910@gmail.com>
2023-07-12 03:40:01 +0900

    마이페이지 만듦

--- WhyPyProjdect/src/main/java/com/example/whypyprojdect/controller/MemberController.java
+++ WhyPyProjdect/src/main/java/com/example/whypyprojdect/controller/MemberController.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
@@ -45,6 +46,11 @@ public class MemberController {
             //login 실패
             return "login";
         }
+    }
+
+    @RequestMapping("/member/MyPage")
+    public String showMyPage() {
+        return "MyPage";
     }
 }
 
