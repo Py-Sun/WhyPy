6bbd5fe3d8b0c9882afffd452742c95af3dcb5d1
subin <ksb3458@naver.com>
2023-07-16 22:25:07 +0900

    볼드체 변경

--- WhyPyProjdect/src/main/resources/static/js/create-post.js
+++ WhyPyProjdect/src/main/resources/static/js/create-post.js
@@ -8,13 +8,16 @@ function a() {
 function bold() {
     boldCnt++;
     let button = document.getElementById('boldBtn');
-    let contents=document.getElementById('contents').value;
+    let contents = document.getElementById('contents').value;
+    let element = document.getElementById('contents');
     if(boldCnt % 2 == 1) {
         button.style.backgroundColor = "blue"
-        document.getElementById('contents').value = contents + "<b>";
+        document.getElementById('contents').value = contents + "**";
+        //element.style.fontWeight = "bold";
     }
     else {
         button.style.backgroundColor = "lightgrey"
-        document.getElementById('contents').value = contents + "</b>";
+        document.getElementById('contents').value = contents + "**";
+        //element.style.fontWeight = "normal";
     }
 }
