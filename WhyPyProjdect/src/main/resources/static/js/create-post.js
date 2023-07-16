e374f868dec794a90ec47d96b3911358fe2c5ad6
subin <ksb3458@naver.com>
2023-07-17 02:11:49 +0900

    코드 삽입 테스트

--- WhyPyProjdect/src/main/resources/static/js/create-post.js
+++ WhyPyProjdect/src/main/resources/static/js/create-post.js
@@ -1,23 +1,42 @@
 boldCnt = 0;
-
-function a() {
-    boldCnt++;
-    console.log(boldCnt);
-}
+codeCnt = 0;
 
 function bold() {
     boldCnt++;
     let button = document.getElementById('boldBtn');
+    let contentElement = document.getElementById('contents');
     let contents = document.getElementById('contents').value;
-    let element = document.getElementById('contents');
     if(boldCnt % 2 == 1) {
-        button.style.backgroundColor = "blue"
-        document.getElementById('contents').value = contents + "**";
+        button.style.backgroundColor = "blue";
         //element.style.fontWeight = "bold";
     }
     else {
-        button.style.backgroundColor = "lightgrey"
-        document.getElementById('contents').value = contents + "**";
+        button.style.backgroundColor = "lightgrey";
         //element.style.fontWeight = "normal";
     }
+    document.getElementById('contents').value = contents + "**";
+    let pos = contentElement.value.length;
+    contentElement.selectionStart = pos;
+    contentElement.selectionEnd = pos;
+    contentElement.focus();
+}
+
+function code() {
+    codeCnt++;
+    let button2 = document.getElementById('codeBtn');
+    let contentElement = document.getElementById('contents');
+    let contents = document.getElementById('contents').value;
+    if(boldCnt % 2 == 1) {
+        button2.style.backgroundColor = "blue";
+        //element.style.fontWeight = "bold";
+    }
+    else {
+        button2.style.backgroundColor = "lightgrey";
+        //element.style.fontWeight = "normal";
+    }
+    document.getElementById('contents').value = contents + "'''";
+    let pos = contentElement.value.length;
+    contentElement.selectionStart = pos;
+    contentElement.selectionEnd = pos;
+    contentElement.focus();
 }
