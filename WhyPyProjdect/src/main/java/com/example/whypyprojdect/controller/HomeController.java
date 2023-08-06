c1888769f8fc1be8391d0611b7085a3535222f0c
Hyun_W <69100145+yehang218@users.noreply.github.com>
2023-08-06 15:32:54 +0900

    📌 로그인 후 이전 페이지로 / 로그인 시 로그아웃 버튼으로

--- WhyPyProjdect/src/main/java/com/example/whypyprojdect/controller/HomeController.java
+++ WhyPyProjdect/src/main/java/com/example/whypyprojdect/controller/HomeController.java
@@ -1,6 +1,9 @@
 package com.example.whypyprojdect.controller;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,7 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     //GET, POST 모두
     @GetMapping("/")
-    public String home() {
+    public String home(HttpServletRequest request, Model model) {
+        HttpSession session= request.getSession();
+        if (!session.isNew() && session.getAttribute("loginName") !=null) {
+
+            String myName = (String) session.getAttribute("loginName");
+            System.out.println(myName);
+            model.addAttribute("loginName",myName);
+        }
         return "home";
     }
 
