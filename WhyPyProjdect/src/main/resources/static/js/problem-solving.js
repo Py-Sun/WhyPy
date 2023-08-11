78f3e1720b09c5b41d66da094f437dcf2f3f47b0
Hyun_W <69100145+yehang218@users.noreply.github.com>
2023-08-11 20:33:30 +0900

    📌 문제 풀기 - 작성한 코드 DB 저장

--- WhyPyProjdect/src/main/resources/static/js/problem-solving.js
+++ WhyPyProjdect/src/main/resources/static/js/problem-solving.js
@@ -10,6 +10,29 @@ function initCodeMirror() {
     document.getElementById("reset-button").addEventListener("click", () => {
         editor.setValue("코드를 입력해주세요. ");
     });
+    
+    // 제출 버튼 클릭
+    document.getElementById("submitAnswer").addEventListener("click", () =>{
+        var userAnswer = editor.getValue();
+        var questionId = $("#questionId").val();
+
+        if (userAnswer === "코드를 입력해주세요. ") {
+            userAnswer = ""; // 코드가 초기 값이면 공백으로 설정
+        }
+
+        // AJAX 요청 보내기
+        $.ajax({
+            url: "/questions/" + questionId,
+            method: "POST",
+            data: {
+                userAnswer: userAnswer
+            },
+            success: function(data) {
+                // AJAX 요청이 성공적으로 완료되었을 때 실행되는 콜백 함수
+                window.location.href = data; // 리다이렉트 수행
+            }
+        });
+    });
 }
 
 // 페이지 로드 시 CodeMirror 초기화 함수 호출
