b42c9c894c047dec7cbdc451159383cab4177f79
ksb3458 <86918962+ksb3458@users.noreply.github.com>
2023-09-06 16:24:11 +0900

    게시글 작성 페이지 연결 (2)

--- WhyPyProjdect/src/main/resources/static/js/create-post.js
+++ WhyPyProjdect/src/main/resources/static/js/create-post.js
@@ -6,7 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
     contentsElement.addEventListener("input", function() {
         const boldText = this.value.replace(/\*\*(.*?)\*\*/g, '<span style="font-weight: bold;">$1</span>');
         this.innerHTML = boldText;
-        console.log(contentsElement.innerHTML);
+        //console.log(contentsElement.innerHTML);
     });
 });
 
@@ -15,14 +15,14 @@ function bold() {
     let button = document.getElementById('boldBtn');
     let contentElement = document.getElementById('contents');
     let contents = document.getElementById('contents').value;
-    if(boldCnt % 2 == 1) {
-        button.style.backgroundColor = "blue";
-        //element.style.fontWeight = "bold";
-    }
-    else {
-        button.style.backgroundColor = "lightgrey";
-        //element.style.fontWeight = "normal";
-    }
+    // if(boldCnt % 2 == 1) {
+    //     button.style.backgroundColor = "blue";
+    //     //element.style.fontWeight = "bold";
+    // }
+    // else {
+    //     button.style.backgroundColor = "lightgrey";
+    //     //element.style.fontWeight = "normal";
+    // }
     document.getElementById('contents').value = contents + "**";
     let pos = contentElement.value.length;
     contentElement.selectionStart = pos;
@@ -35,14 +35,14 @@ function code() {
     let button2 = document.getElementById('codeBtn');
     let contentElement = document.getElementById('contents');
     let contents = document.getElementById('contents').value;
-    if(codeCnt % 2 == 1) {
-        button2.style.backgroundColor = "blue";
-        //element.style.fontWeight = "bold";
-    }
-    else {
-        button2.style.backgroundColor = "lightgrey";
-        //element.style.fontWeight = "normal";
-    }
+    // if(codeCnt % 2 == 1) {
+    //     button2.style.backgroundColor = "blue";
+    //     //element.style.fontWeight = "bold";
+    // }
+    // else {
+    //     button2.style.backgroundColor = "lightgrey";
+    //     //element.style.fontWeight = "normal";
+    // }
     document.getElementById('contents').value = contents + "'''";
     let pos = contentElement.value.length;
     contentElement.selectionStart = pos;
