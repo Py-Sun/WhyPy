bc3e78c97f6ff8cf0fa2dd76bce0f613fde9bbf0
kimhyunseoo <137757602+kimhyunseoo@users.noreply.github.com>
2023-10-10 20:55:35 +0900

    문제풀이페이지 버튼,박스수정

--- WhyPyProjdect/run.py
+++ WhyPyProjdect/run.py
@@ -1,1 +1,13 @@
-코드를 입력해주세요.
+N = 3  # Number of elements
+lists = [5, 7, 7]  # List of elements
+cnt = 0
+
+while max(lists) != lists[0]:
+    lists[lists.index(max(lists))] -= 1
+    lists[0] += 1
+    cnt += 1
+
+if lists.count(max(lists)) >= 2:
+    print(cnt + 1)
+else:
+    print(cnt)
